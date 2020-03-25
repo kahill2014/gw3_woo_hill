e20476e43c100cc9eb5491c07ef1e9e29896cf09
Kevin Hill <kahill2014@gmail.com>
2020-03-25 11:11:33 -0500

    Started adding thumbnails and added lightbox gallery plugin

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 $.backstretch("../images/good-place-images/good-place-background.jpg");
 $(".backstretch").css("opacity", ".1");
+$(document).ready(function() {
+	$("#lightgallery").lightGallery(); 
+});
