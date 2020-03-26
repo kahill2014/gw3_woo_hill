1cac245a00dfd478777dbd9fc9b18bcaf6d1313b
Kevin Hill <kahill2014@gmail.com>
2020-03-26 09:59:13 -0500

    Added bad place styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-$.backstretch('../images/good-place-images/good-place-background.jpg');
+if($('.good-place').length){
+	$.backstretch('../images/good-place-images/good-place-background.jpg');
+} else if($('.bad-place').length) {
+	$.backstretch('../images/bad-place-images/bad-place-background.jpg');
+}
+
 $(document).ready(function() {
 	$('#lightgallery').lightGallery(); 
 });
