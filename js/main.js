dd7f6c404b3660ce841aed219588350d5c71d724
Kevin Hill <kahill2014@gmail.com>
2020-04-14 11:14:56 -0500

    Added ability to hover on label or door to trigger animation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
+/* Assign background images utilizing the Backstretch plugin */
 if($('.good-place').length){
 	$.backstretch('../images/good-place-images/good-place-background.jpg');
 } else if($('.bad-place').length) {
 	$.backstretch('../images/bad-place-images/bad-place-background.jpg');
 }
-
+/* Initialize light gallery on the good/bad place pages */
 $(document).ready(function() {
 	$('#lightgallery').lightGallery();
 });
+/* Initialize the Slick plugin for testimonial slides */
 $('.carousel').slick({
 	dots: true,
 	infinite: true,
 	autoplay: true,
 	autoplaySpeed: 5500
 });
+/* Call hover on navigation doors when their label is hovered */
+$('.nav-item a').hover(function(){
+	toggleHover(this);
+}, function(){
+	toggleHover(this);
+});
+/* Toggles hover class for navigation elements - avoids repeating code */
+function toggleHover(e){
+	var nav_img = $(e).find('img');
+	if (nav_img.hasClass('hover-bad') || nav_img.hasClass('hover-lobby') || nav_img.hasClass('hover-good')){
+		nav_img.removeClass();
+	} else {
+		var nav_type = $(e).attr('class').split('-')[1];
+		console.log(nav_type);
+		nav_img.addClass('hover-' + nav_type);
+	}
+}
